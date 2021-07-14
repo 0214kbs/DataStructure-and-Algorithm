087df76cba0e8df56f8911237cda73c7f107eae0
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-07-14 23:41:05 +0900

    Update 완주하지 못한 선수.java

--- hash/완주하지 못한 선수.java
+++ hash/완주하지 못한 선수.java
@@ -1,3 +1,9 @@
+/* 프로그래머스 해시 Lv 1 - 완주하지 못한 선수
+ https://programmers.co.kr/learn/courses/30/lessons/42576 */
+
+/* 문제 해설
+https://bskwak.tistory.com/222 참고 
+ */
 import java.util.*;
 
 class Solution {
