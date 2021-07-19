b30270b6249b4b128a1c168fefa7eaf11f9f6eb9
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-07-19 21:43:14 +0900

    1이 될 때 까지 첫 idea : 단순히 1이 될 때까지 반복문을 돌고 문제 그대로의 과정을 실행

---
+++ greedy/1이 될 때 까지.py
@@ -0,0 +1,17 @@
+# N이 1이 될 때까지의 최소 횟수 구하기
+# 과정 1. N에서 1 빼기
+# 과정 2. N에서 K로 나누기 (대신 나누어 떨어져야 함)
+
+n,k = map(int,input().split())
+cnt = 0
+while True:
+    if(n==1):
+        break
+    if(n%k==0) :
+        n//=k
+        cnt+=1
+    else :
+        n-=1
+        cnt+=1
+
+print(cnt)
