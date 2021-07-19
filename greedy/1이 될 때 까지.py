3b835ca112a03ae1118965685723c32e161d5bdd
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-07-19 21:54:09 +0900

    1이 될 때 까지 첫 idea : 단순히 1이 될 대까지 반복문을 돌고 문제 그대로의 과정을 실행
    + 성능 평가를 위해 시간 측정 코드 도입

--- greedy/1이 될 때 까지.py
+++ greedy/1이 될 때 까지.py
@@ -1,6 +1,10 @@
 # N이 1이 될 때까지의 최소 횟수 구하기
 # 과정 1. N에서 1 빼기
 # 과정 2. N에서 K로 나누기 (대신 나누어 떨어져야 함)
+from random import randint
+import time
+
+start_time = time.time()
 
 n,k = map(int,input().split())
 cnt = 0
@@ -15,3 +19,7 @@ while True:
         cnt+=1
 
 print(cnt)
+
+end_time = time.time()
+
+print("시간 :", end_time - start_time)
