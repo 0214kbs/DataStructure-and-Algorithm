a09b5761dc78ac33ae8eb183c423b48ff77428fb
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-08-26 15:06:58 +0900

    Baekjoon 4673 self number

--- Baekjoon/4673 셀프 넘버.py
+++ Baekjoon/4673 셀프 넘버.py
@@ -5,6 +5,7 @@ def d(n):
     return int(n) + cnt
 
 
+# cnt = 0
 check = []
 for i in range(1, 10001):
     n = d(i)
@@ -15,3 +16,30 @@ for i in range(1, 10001):
         pass
     else:
         print(i)
+        # cnt = cnt+1
+s
+# print(cnt)
+
+
+# wrong code -> need to correct
+
+# def d(n):
+#     cnt = n
+#     while n != 0:
+#         cnt = cnt + (n % 10)
+#         n = n / 10
+#     return int(cnt)
+#
+# cnt = 0
+# check = [False for i in range(10001)]
+# for i in range(1, 10001):
+#     n = d(i)
+#     if n < 10001:
+#         check[n]=True
+#
+# for i in range(1, 10001):
+#     if not check[i]:
+#         print(i)
+#         cnt = cnt+1
+#
+# print(cnt)
