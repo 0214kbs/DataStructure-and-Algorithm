1bb3dc357dc81b7b104dd4348f142e1871711b9d
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-08-27 18:29:13 +0900

    Baekjoon 11729 hanoi top

---
+++ Recursion/B11729.py
@@ -0,0 +1,12 @@
+def hanoi_top(n, a, b, c):
+    if n == 1:
+        print(a, c)
+    else:
+        hanoi_top(n - 1, a, c, b)
+        print(a, c)
+        hanoi_top(n - 1, b, a, c)
+
+
+n = int(input())
+print(2**n-1)
+hanoi_top(n, 1, 2, 3)
