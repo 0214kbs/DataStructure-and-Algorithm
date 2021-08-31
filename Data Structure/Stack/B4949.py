5a5960457bfa07ceab87fc75baf8e23e20bb29c8
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-08-31 15:49:37 +0900

    need to correct

--- Data Structure/Stack/B4949.py
+++ Data Structure/Stack/B4949.py
@@ -1,34 +1,43 @@
-import sys
+def push(stack, x):
+    stack.append(x)
 
 
-def balance_world(string):
-    dic = {
-        ')':'(',
-        ']':'['
-    }
-    stack = []
+def pop(stack):
+    stack.pop()
 
-    for c in string:
-        if c in dic.values():
-            stack.append(c)
-            # print(stack)
-        elif c in dic and stack:
-            if stack[-1] == dic[c]:
-                # print(c)
-                stack.pop()
+
+def top(stack):
+    return stack[-1] if stack else -1
+
+
+dic = {
+    ')': '(',
+    ']': '['
+}
+
+while True:
+    string = input()
+    if string == '.':
+        break
+    else:
+        stack = []
+        print_value = True
+
+        for c in string:
+            if c in dic.values():
+                # stack.append(c)
+                push(stack, c)
                 # print(stack)
-            else:
-                return False
-        if c == '.' :
-            if not stack:
-                return True
-            else:
-                return False
-
-
-# s = sys.stdin.readline
-s = input()
-if balance_world(s):
-    print("yes")
-else:
-    print("no")
+            elif c in dic.keys():
+                if top(stack) == dic[c] and len(stack)!=0:
+                    # stack.pop()
+                    pop(stack)
+                    # print(stack)
+                else:
+                    if top(stack) == dic.keys():
+                        push(stack,c)
+                        break
+        if len(stack) == 0:
+            print("yes")
+        else:
+            print("no")
