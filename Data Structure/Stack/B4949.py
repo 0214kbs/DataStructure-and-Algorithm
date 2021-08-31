8a06498ed6e2bf8ab5217e6288d93ee9605309e8
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-08-31 14:42:26 +0900

    need to correct

--- Data Structure/Stack/B4949.py
+++ Data Structure/Stack/B4949.py
@@ -2,20 +2,33 @@ import sys
 
 
 def balance_world(string):
+    dic = {
+        ')':'(',
+        ']':'['
+    }
     stack = []
+
     for c in string:
-        if c == '.':
-            break
-        if c in '({[':
+        if c in dic.values():
             stack.append(c)
-        elif c in ')}]' and len(stack) != 0:
-            stack.pop()
-    if len(stack) == 0:
-        print("yes")
-    else:
-        print("no")
+            # print(stack)
+        elif c in dic and stack:
+            if stack[-1] == dic[c]:
+                # print(c)
+                stack.pop()
+                # print(stack)
+            else:
+                return False
+        if c == '.' :
+            if not stack:
+                return True
+            else:
+                return False
 
 
 # s = sys.stdin.readline
 s = input()
-balance_world(s)
+if balance_world(s):
+    print("yes")
+else:
+    print("no")
