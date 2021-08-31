908c840b6143826e8f0fd76b19a0fe6a17d3a313
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-09-01 01:01:00 +0900

    hash : linear data structure

--- Linear Data Structure/Stack/implement_with_Queue.py
+++ Linear Data Structure/Stack/implement_with_Queue.py
@@ -5,6 +5,7 @@ class Stack:
     def __init__(self):
         self.q = collections.deque()
 
+    #queue를 이용해 재정렬
     def push(self, x):
         self.q.append(x)
         for _ in range(len(self.q) - 1):
