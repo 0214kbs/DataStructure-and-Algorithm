4840ad92c242611a3377d9393a48b99c34596a09
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-09-01 10:19:08 +0900

    Implement Queue with Stack

--- Linear Data Structure/Queue/implement_with_Stack.py
+++ Linear Data Structure/Queue/implement_with_Stack.py
@@ -19,3 +19,15 @@ class Queue:
 
     def empty(self):
         return self.input == [] and self.output == []
+
+q = Queue()
+q.push(1)
+q.push(2)
+print(q.peek())
+print(q.pop())
+print(q.peek())
+q.pop()
+if q.empty():
+    print("empty")
+else:
+    print("not empty")
