9e8aa4c15f6552187ae743be95d3d4682ab9b55c
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-09-01 14:49:31 +0900

    Implement of Linked List with C language

--- Linear Data Structure/Linked List/Implement_LinkedList/Implement_LinkedList/main.c
+++ Linear Data Structure/Linked List/Implement_LinkedList/Implement_LinkedList/main.c
@@ -12,67 +12,118 @@
 typedef struct NODE {
     
     int data;             // 데이터를 저장할 변수
-    struct NODE* next;    // 다음 노드의 주소를 저장할 포인터(자기참조 구조체 포인터) : "노드 구조체의 주소" 저장
-    
-    
-}node;
+    struct NODE* link;    // 다음 노드의 주소를 저장할 포인터(자기참조 구조체 포인터) : "노드 구조체의 주소" 저장
+      
+}NODE;
 
 //n번째 노드에 저장되어 있는 값을 불러오는 함수
 char get(struct NODE *target,int n){
     
     for(int i=0;i<n;i++){
-        target=target->next;
+        target=target->link;
     }
     return target->data;
 }
 
 // 기준 노드 뒤에 노드를 추가하는 함수
-void insertNode(struct NODE *target, int data){
+void insert(struct NODE *head, int pos, int data){
     
-    node* newNode = (node*)malloc(sizeof(node));    // new node
-    newNode->next = target->next;                   // 새 노드의 다음 노드에 기준 노드의 다음 노드를 지정
-    newNode->data = data;                           // 데이터 저장
-    target->next = newNode;                         // 기준 노드의 다음 노드에 새 노드를 지정
+    NODE* cur = head;
+    NODE* new = malloc(sizeof(NODE));             // new node
+    new->data = data;                             // 데이터 저장
+    new->link = NULL;
+    
+    if (pos == 0){
+        new->link = head->link;                   // head 노드의 다음 노드를 지정
+        head->link = new;                         // head 노드의 다음 노드에 새 노드를 지정
+    }
+    else{
+        int cnt = 0;
+        while(cnt != pos){
+            if(cnt == (pos-1)){
+                new->link = cur->link;             // 새 노드의 다음 노드에 기준 노드의 다음 노드를 지정
+                cur->link = new;                   // 기준 노드의 다음 노드에 새 노드를 지정
+            }
+            cur = cur->link;
+            cnt++;
+        }
+    }
     
 }
 
 // 기준 노드의 다음 노드를 삭제하는 함수
-void deleteFirst(struct NODE *target){
+void delete(struct NODE *head, int data){
     
-    node* deleteNode = target->next;     // 기준 노드의 다음 노드 주소를 저장
-    target->next = deleteNode->next;     // 기준 노드의 다음 노드에 삭제할 노드의 다음 노드를 지정
-    free(deleteNode);    // 노드 메모리 해제
+    NODE* cur = head;
+    NODE* pre = cur;
+    
+    while(cur->data != data){
+        pre = cur;
+        cur = cur->link;
+    }
+    
+    pre->link = cur->link;          // 기준 노드의 다음 노드에 삭제할 노드의 다음 노드를 지정
+    free(cur);                      // 노드 메모리 해제
+    
+}
+
+void reverse(NODE * head){
+    NODE *p, *q, *r;
+    p = head->link;
+    q=NULL;
+    
+    while(p!=NULL){
+        r=q;
+        q=p;
+        p=p->link;
+        q->link=r;
+    }
+    head->link = q;
+}
+
+void print_list(NODE* head){
+    NODE *cur = head->link;
+    while(cur!=NULL){
+        printf("%d",cur->data);
+        cur = cur->link;
+    }
+    printf("\n");
+    free(cur);
 }
 
 int main(){
     
-    node *head = (node*)malloc(sizeof(node));    // head 노드 생성
-    // head 노드는 데이터를 저장하지 않음
-    
-    head->next = NULL;
-    
-    insertNode(head, 10);                         // head 노드 뒤에 새 노드 추가
-    insertNode(head, 20);                         // head 노드 뒤에 새 노드 추가
-    insertNode(head, 30);                         // head 노드 뒤에 새 노드 추가
+        NODE *head = malloc(sizeof(NODE));
 
-    deleteFirst(head);                          // head 노드 뒤에 있는 노드를 삭제
+        NODE *node1 = malloc(sizeof(NODE));
+        head->link = node1;
+        node1->data = 10;
 
-    node* curr = head->next;                    // 순회용 node 생성 : 연결 리스트 순회용 포인터에 첫 번째 노드의 주소 저장
-    while (curr != NULL){                       // 포인터가 NULL이 아닐 때 계속 반복
-        printf("%d\n", curr->data);             // 현재 노드의 데이터 출력
-        curr = curr->next;                      // 포인터에 다음 노드의 주소 저장
-    }
+        NODE *node2 = malloc(sizeof(NODE));
+        node2->data = 20;
+        node1->link = node2;
+        node2->link = NULL;
+
+        // Initiaized linked list
+        printf("Original linked list:        ");
+        print_list(head);
+
+        // After insert 30 on index 1 in linked list
+        insert(head, 1, 30);
+        printf("Ater insert 30 in the list:  ");
+        print_list(head);
+
+        // After delete 10 in the linked list
+        delete(head, 10);
+        printf("After delete 10 in the list: ");
+        print_list(head);
+
+        // After reversing the linked_list
+        reverse(head);
+        printf("After reversing the list:    ");
+        print_list(head);
+        
+        return 0;
+
     
-    printf("%d\n", get(head,1));
-    printf("%d\n", get(head,2));
-    
-    while (curr != NULL){                       // 포인터가 NULL이 아닐 때 계속 반복
-        node *next = curr->next;                // 현재 노드의 다음 노드 주소를 임시로 저장
-        free(curr);                             // 현재 노드 메모리 해제
-        curr = next;                            // 포인터에 다음 노드의 주소 저장
-    }
-
-    free(head);                                 // head 노드 메모리 해제
-
-    return 0;
 }
