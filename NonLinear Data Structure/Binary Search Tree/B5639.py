8042318511703f4a7369408097ba5464ccc93137
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-09-10 14:03:32 +0900

    백준 5639번 : 이진검색트리

--- NonLinear Data Structure/Binary Search Tree/B5639.py
+++ NonLinear Data Structure/Binary Search Tree/B5639.py
@@ -1,5 +1,20 @@
-#stack 사용하려다 실패
-tree = {}
+import sys
+sys.setrecursionlimit(10*6)
+input = sys.stdin.readline
+
+def postOrder(n):
+    length = len(n)
+
+    if length <= 1:
+        return n
+
+    for i in range(1, length):
+        if n[i] > n[0]:
+            return postOrder(n[1:i]) + postOrder(n[i:]) + [n[0]]
+
+    return postOrder(n[1:]) + [n[0]]
+
+
 n = []
 while True:
     try:
@@ -7,34 +22,26 @@ while True:
     except:
         break
 
-# for i in range(1, len(n)+1):
-#     # print(n[i-1])
-#     # print(n[i])
-#     # print("---------------------")
-#     if n[i-1] > n[i]:
-#         stack.append(n[i])
+n = postOrder(n)
+for i in n:
+    print(i)
+
+# stack = []
+# stack2 = []
+#
+# while True:
+#     i = 1
+#     if n[i] > n[0]:
+#         stack2.append(n[i])
 #     else:
-#         if len(stack) != 1:
-#             print(stack.pop())
-#             print(n[i])
-#         else:
-#             print(stack.pop())
-stack = []
-stack2 = []
-
-while True:
-    i = 1
-    if n[i] > n[0]:
-        stack2.append(n[i])
-    else:
-        stack.append(n[i])
-    i = i + 1
-
-stack.sort()
-
-while True:
-    if len(stack) != 0:
-        print(stack.pop())
-    else:
-        if len(stack2) != 0:
-            print(stack2.pop())
+#         stack.append(n[i])
+#     i = i + 1
+#
+# stack.sort()
+#
+# while True:
+#     if len(stack) != 0:
+#         print(stack.pop())
+#     else:
+#         if len(stack2) != 0:
+#             print(stack2.pop())
