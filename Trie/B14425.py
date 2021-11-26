2a78f665b30a0f10f724e297efadcf2a1204aa77
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-11-26 19:36:08 +0900

    백준 14425번 문자열 집합 - trie로 문제 해결

---
+++ Trie/B14425.py
@@ -0,0 +1,53 @@
+import sys
+
+input = sys.stdin.readline
+
+
+class Node(object):
+    def __init__(self, key, data=None):
+        self.key = key
+        self.data = data
+        self.children = {}
+
+
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]
+        current_node.data = string
+
+    def search(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char in current_node.children:
+                current_node = current_node.children[char]
+            else:
+                return False
+        if current_node.data:
+            return True
+        else:
+            return False
+
+
+n, m = map(int, input().split())
+trie = Trie()
+cnt = 0
+
+for i in range(n + m):
+    # input을 그대로 사용하면 "TypeError 'builtin_function_or_method' object is not iterable" 에러 발생
+    string = input().rstrip()  # 따라서 뒤 공백문자 제거해주어야함
+
+    if i < n:
+        trie.insert(string)
+    else:
+        if trie.search(string):
+            cnt += 1
+print(cnt)
